6b8fdffca054bf393480d737e0d85bba40970a37
imademo <xelobpgm@gmail.com>
2026-03-11 11:45:30 +0100

    Controller Commande (non fini, je vais pull chez moi et continuer)

---
+++ Backend/APICube/APICube/Models/Repository/IDataRepositoryCommande.cs
@@ -0,0 +1,9 @@
+﻿using APICube.Models.EntityFramework;
+using static APICube.Models.Repository.IDataRepository;
+
+namespace APICube.Models.Repository
+{
+    public interface IDataRepositoryCommande : IDataRepository<Commande>
+    {
+    }
+}
