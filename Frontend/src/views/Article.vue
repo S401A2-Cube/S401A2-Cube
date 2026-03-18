7844c5c887c64d118b2814fc14b6aee41860fb2f
imademo-png <bassimi@D250-S11>
2026-03-18 11:19:37 +0100

    pas fini

--- Frontend/src/views/Article.vue
+++ Frontend/src/views/Article.vue
@@ -9,24 +9,32 @@ const BikesCategories = {
     "Trekking": ["Vue d'ensemble", "Tour", "City", "Variantes de cadres"]
 };
 const filtres = ['Variante Cadre', 'Millésime', 'Couleurs'];
-
+// const chevron = document.getElementById('chevron');
+// chevron.addEventListener('click',() =>{chevron.ClassList="rotated"});
 
 const menuItems = [
   { name: 'Bike', cats: BikesCategories },
 
 ];
 
+
 var selected = ref(null);
 
+
+const selectedIndex = ref(null);
+
+const toggleRotation = (index) => {
+  selectedIndex.value = selectedIndex.value === index ? null : index;
+}
 </script>
 
 <template>
     <h1>Découvrez nore sélection</h1>
     <div id="Side_Filtre">
         <div v-for="titre in filtres">
-            <div class="side_filtre">
+            <div class="side_filtre"  >
                <h2>{{titre}}</h2>
-               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="chevron"><path d="m18 15-6-6-6 6"/></svg>
+               <svg xmlns="http://www.w3.org/2000/svg" @click="toggleRotation(index)" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="chevron" :class="{ 'rotated': selectedIndex === index }" ><path d="m18 15-6-6-6 6"/></svg>
             </div>
         </div>
                 
@@ -34,6 +42,15 @@ var selected = ref(null);
 
         
     </div>
+
+
+    <div id="articleContainer">
+    <Product 
+      v-for="product in BikesCategories"
+      :info="product"
+
+      />
+  </div>
 
 </template>
 
@@ -43,6 +60,7 @@ var selected = ref(null);
     position: absolute;
     left: 50px;
     
+    
 }
 
 .side_filtre {
@@ -51,11 +69,17 @@ var selected = ref(null);
     justify-content: space-between; 
     cursor: pointer;
     padding: 5px 0;
+    
 }
 
 #Side_Filtre h2 {
    font-weight: bold;
+   
     
 }
-.chevron {}
+
+.chevron {
+    transition: transform 0.5s ease;
+}
+.rotated{transform: rotate(180deg)}
 </style>
