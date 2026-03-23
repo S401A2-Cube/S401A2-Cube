924371f3d539694492052f8cda653bc9eb2687ac
imademo <xelobpgm@gmail.com>
2026-03-23 03:58:27 +0100

    Couleurs Table

---
+++ Backend/S401A2/Controllers/CouleurController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using S401A2.Model.EntityFramework;
+using S401A2.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S401A2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouleursController : ControllerBase
+    {
+        private readonly IDataRepository<Couleur> _repository;
+
+        public CouleursController(IDataRepository<Couleur> dataRepository)
+        {
+            _repository = dataRepository ?? throw new ArgumentNullException(nameof(dataRepository));
+        }
+
+        // GET: api/Couleurs
+        [HttpGet]
+        [ActionName("GetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IEnumerable<Couleur>> GetCouleurs()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        // GET: api/Couleurs/5
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Couleur>> GetCouleur(int id)
+        {
+            var couleur = await _repository.GetByIdAsync(id);
+
+            if (couleur == null)
+            {
+                return NotFound();
+            }
+
+            return couleur;
+        }
+
+        // PUT: api/Couleurs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PutCouleur(int id, Couleur couleur)
+        {
+            if (id != couleur.IdCouleur)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var couleurToUpdate = await _repository.GetByIdAsync(id);
+            if (couleurToUpdate == null || couleurToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.UpdateAsync(couleurToUpdate, couleur);
+
+            return NoContent();
+        }
+
+        // POST: api/Couleurs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Couleur>> PostCouleur(Couleur couleur)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _repository.AddAsync(couleur);
+
+            return CreatedAtAction("GetCouleur", new { id = couleur.IdCouleur }, couleur);
+        }
+
+        // DELETE: api/Couleurs/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteCouleur(int id)
+        {
+            var couleur = await _repository.GetByIdAsync(id);
+            if (couleur == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.DeleteAsync(couleur);
+
+            return NoContent();
+        }
+    }
+}
