a68fd36c932b7010e18b09527ce3f1abd2a8768c
Auria-dev <64524466+Auria-dev@users.noreply.github.com>
2026-03-31 13:54:25 +0200

    removed bad images

--- Frontend/src/components/NavBar.vue
+++ Frontend/src/components/NavBar.vue
@@ -40,7 +40,7 @@ var selected = ref(null);
                     :categories="item.cats"
                 />
                 <router-link to="/Article">
-                    <img src="../img/loupe.png" width="40" height="40" alt="loupe" @click="console.log('test')">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
                 </router-link>
             </div>
             <div class="navbar_right">
