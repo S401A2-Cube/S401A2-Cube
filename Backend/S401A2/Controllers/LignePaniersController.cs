2b2868fdd9f347dfb6e6dddf746c63fe14dc8e4d
SonicRaft29-etu <tiago-rafael.belchior-dias@etu.univ-smb.fr>
2026-03-31 16:10:14 +0200

    Ajouter controller et manager de lignepanier + modifier lignepanier

---
+++ Backend/S401A2/Controllers/LignePaniersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using S401A2.Model.EntityFramework;
+
+namespace S401A2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LignePaniersController : ControllerBase
+    {
+        private readonly CubeDBContext _context;
+
+        public LignePaniersController(CubeDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LignePaniers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LignePanier>>> GetLignePaniers()
+        {
+            return await _context.LignePaniers.ToListAsync();
+        }
+
+        // GET: api/LignePaniers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LignePanier>> GetLignePanier(int id)
+        {
+            var lignePanier = await _context.LignePaniers.FindAsync(id);
+
+            if (lignePanier == null)
+            {
+                return NotFound();
+            }
+
+            return lignePanier;
+        }
+
+        // PUT: api/LignePaniers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLignePanier(int id, LignePanier lignePanier)
+        {
+            if (id != lignePanier.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lignePanier).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LignePanierExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LignePaniers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<LignePanier>> PostLignePanier(LignePanier lignePanier)
+        {
+            _context.LignePaniers.Add(lignePanier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLignePanier", new { id = lignePanier.Id }, lignePanier);
+        }
+
+        // DELETE: api/LignePaniers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLignePanier(int id)
+        {
+            var lignePanier = await _context.LignePaniers.FindAsync(id);
+            if (lignePanier == null)
+            {
+                return NotFound();
+            }
+
+            _context.LignePaniers.Remove(lignePanier);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LignePanierExists(int id)
+        {
+            return _context.LignePaniers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Backend/S401A2/Model/DataManager/LignePanierManager.cs
@@ -0,0 +1,157 @@
+﻿using APICube.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using S401A2.Model.EntityFramework;
+using S401A2.Models.Repository;
+
+namespace S401A2.Model.DataManager
+{
+    public class LignePanierManager : IDataRepository<LignePanier>
+    {
+        private readonly CubeDBContext? _context;
+        public LignePanierManager() { }
+
+        public LignePanierManager(CubeDBContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task AddAsync(LignePanier entity)
+        {
+            if (_context != null)
+            {
+                await _context.LignePaniers.AddAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("Database context is not available.");
+            }
+        }
+
+        public async Task DeleteAsync(LignePanier entity)
+        {
+            if (_context != null)
+            {
+                _context.LignePaniers.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("Database context is not available.");
+            }
+        }
+
+        public async Task<IEnumerable<LignePanier>> GetAllAsync()
+        {
+            if (_context == null)
+            {
+                return null;
+            }
+
+            var lignes = await _context.LignePaniers
+                    .AsNoTracking()
+                    .Select(l => new LignePanier
+                    {
+                        Id = l.Id,
+                        QtePanier = l.QtePanier,
+                        ArticleId = l.ArticleId,
+                        ClientId = l.ClientId,
+                        CommandeId = l.CommandeId,
+                        TailleId = l.TailleId,
+                        CouleurId = l.CouleurId,
+
+                        ArticleLignePanier = l.ArticleLignePanier == null ? null : new Article
+                        {
+                            ArticleId = l.ArticleLignePanier.ArticleId,
+                            Nom = l.ArticleLignePanier.Nom,
+                            Reference = l.ArticleLignePanier.Reference,
+                            Prix = l.ArticleLignePanier.Prix,
+                            ArticleLignePanier = null
+                        },
+
+                        CouleurChoisie = l.CouleurChoisie == null ? null : new Couleur
+                        {
+                            IdCouleur = l.CouleurChoisie.IdCouleur,
+                            NomCouleur = l.CouleurChoisie.NomCouleur,
+                            Velos = null
+                        },
+
+                        TailleChoisie = l.TailleChoisie == null ? null : new Taille
+                        {
+                            IdTaille = l.TailleChoisie.IdTaille,
+                            LibelleTaille = l.TailleChoisie.LibelleTaille,
+                            Velos = null
+                        },
+                    })
+                    .ToListAsync();
+
+            return lignes;
+
+        }
+
+        public async Task<LignePanier?> GetByIdAsync(int id)
+        {
+            if (_context == null)
+            {
+                return null;
+            }
+
+            var ligne = await _context.LignePaniers
+                    .AsNoTracking()
+                    .Where(l => l.Id == id)
+                    .Select(l => new LignePanier
+                    {
+                        Id = l.Id,
+                        QtePanier = l.QtePanier,
+                        ArticleId = l.ArticleId,
+                        ClientId = l.ClientId,
+                        CommandeId = l.CommandeId,
+                        TailleId = l.TailleId,
+                        CouleurId = l.CouleurId,
+
+                        ArticleLignePanier = l.ArticleLignePanier == null ? null : new Article
+                        {
+                            ArticleId = l.ArticleLignePanier.ArticleId,
+                            Nom = l.ArticleLignePanier.Nom,
+                            Reference = l.ArticleLignePanier.Reference,
+                            Prix = l.ArticleLignePanier.Prix,
+                            ArticleLignePanier = null
+                        },
+
+                        CouleurChoisie = l.CouleurChoisie == null ? null : new Couleur
+                        {
+                            IdCouleur = l.CouleurChoisie.IdCouleur,
+                            NomCouleur = l.CouleurChoisie.NomCouleur,
+                            Velos = null
+                        },
+
+                        TailleChoisie = l.TailleChoisie == null ? null : new Taille
+                        {
+                            IdTaille = l.TailleChoisie.IdTaille,
+                            LibelleTaille = l.TailleChoisie.LibelleTaille,
+                            Velos = null
+                        },
+                    })
+                    .FirstOrDefaultAsync();
+
+            return ligne;
+        }
+
+        public Task UpdateAsync(LignePanier entityToUpdate, LignePanier entity)
+        {
+            if (_context != null)
+            {
+                entityToUpdate.QtePanier = entity.QtePanier;
+                entityToUpdate.CouleurChoisie = entity.CouleurChoisie;
+                entityToUpdate.TailleChoisie = entity.TailleChoisie;
+
+                _context.LignePaniers.Update(entityToUpdate);
+                return _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("Database context is not available.");
+            }
+        }
+    }
+}
