949d0c2481a51c605e9af90a2e8594c1f925540e
yodup <yoenie.dupuyy@etu.univ-smb.fr>
2026-04-01 11:46:41 +0200

    Lier données avec API

--- Frontend/src/views/ArticleView.vue
+++ Frontend/src/views/ArticleView.vue
@@ -14,6 +14,8 @@ const utils = useUtilsStore();
 
 const resVelo = ref(null);
 const resArticle = ref(null);
+const selectedColor = ref(null);
+const selectedSize = ref(null);
 
 axios.get(utils.url + "Velos/GetById/" + route.params.id).then(r => {
   const data = r.data;
@@ -51,28 +53,72 @@ axios.get(utils.url + "Velos/GetById/" + route.params.id).then(r => {
     cadres: data.cadres || [],
     geometries: data.geometries || []
   };
+
+  if (resVelo.value.couleurs && resVelo.value.couleurs.length > 0) {
+    selectedColor.value = resVelo.value.couleurs[0].idCouleur;
+  } else {
+    selectedColor.value = null; 
+  }
+
+  if (resVelo.value.tailles && resVelo.value.tailles.length > 0) {
+    selectedSize.value = resVelo.value.tailles[0].idTaille;
+  } else {
+    selectedSize.value = null; 
+  }
 });
 
 const addInShopCart = async () => {
   try {
-    const response = await axios.post('https://votre-api.com/cart', {
-      productId: props.product.id,
-      quantity: 1
-    });
+    const payload = {
+      articleId: resArticle.value.articleId,
+      clientId: 1, 
+      qtePanier: 1,
+      commandeId: null, 
+      couleurId: selectedColor.value, 
+      tailleId: selectedSize.value    
+    };
 
-    console.log('Produit ajouté !', response.data);
-    alert('Produit ajouté au panier');
-
+    await axios.post(utils.url + "LignePaniers/", payload);
+    alert("Ajouté au panier avec succès !");
+    
   } catch (error) {
-    console.error("Erreur lors de l'ajout au panier :", error);
-    alert("Impossible d'ajouter le produit.");
+    console.error("Erreur lors de l'envoi :", error);
   }
 };
 </script>
 
 <template>
   <div class="page-container" v-if="resArticle && resVelo">
-    <Landing :article="resArticle" :velo="resVelo" />
+    <Landing :article="resArticle" :velo="resVelo" />    
+    <div class="selection-group" v-if="resVelo">
+      <div class="option">
+        <span>Couleur :</span>
+        <div class="chips">
+          <button 
+            v-for="c in resVelo.couleurs" 
+            :key="c.idCouleur"
+            :class="{ active: selectedColor === c.idCouleur }"
+            @click="selectedColor = c.idCouleur"
+          >
+            {{ c.nomCouleur }}
+          </button>
+        </div>
+      </div>
+
+      <div class="option">
+        <span>Taille :</span>
+        <div class="chips">
+          <button 
+            v-for="t in resVelo.tailles" 
+            :key="t.idTaille"
+            :class="{ active: selectedSize === t.idTaille }"
+            @click="selectedSize = t.idTaille"
+          >
+            {{ t.libelleTaille }}
+          </button>
+        </div>
+      </div>
+    </div>
     <RedButton @click="addInShopCart">Ajouter au panier</RedButton>
 
     <main id="about">
