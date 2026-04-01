b22765d80ed3f0fa7b4bc81e97b35878f854aab8
NatisUniv <natis.saillet@etu.univ-smb.fr>
2026-04-01 10:30:08 +0200

    Améliorer controller lignepaniers

--- Backend/S401A2/Controllers/LignePaniersController.cs
+++ Backend/S401A2/Controllers/LignePaniersController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using S401A2.Model.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace S401A2.Controllers
 {
@@ -22,13 +23,19 @@ namespace S401A2.Controllers
 
         // GET: api/LignePaniers
         [HttpGet]
+        [ActionName("GetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<LignePanier>>> GetLignePaniers()
         {
             return await _context.LignePaniers.ToListAsync();
         }
 
         // GET: api/LignePaniers/5
-        [HttpGet("{id}")]
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<LignePanier>> GetLignePanier(int id)
         {
             var lignePanier = await _context.LignePaniers.FindAsync(id);
@@ -43,7 +50,11 @@ namespace S401A2.Controllers
 
         // PUT: api/LignePaniers/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Authorize]
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PutLignePanier(int id, LignePanier lignePanier)
         {
             if (id != lignePanier.Id)
@@ -74,7 +85,10 @@ namespace S401A2.Controllers
 
         // POST: api/LignePaniers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Authorize]
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<LignePanier>> PostLignePanier(LignePanier lignePanier)
         {
             _context.LignePaniers.Add(lignePanier);
@@ -84,7 +98,10 @@ namespace S401A2.Controllers
         }
 
         // DELETE: api/LignePaniers/5
+        [Authorize]
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteLignePanier(int id)
         {
             var lignePanier = await _context.LignePaniers.FindAsync(id);
